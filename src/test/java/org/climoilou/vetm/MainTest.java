9a112ef5d8960484822b891019366b161e983134
RafaelTherrien <2362461@cegeplimoilou.ca>
2026-04-27 11:00:45 -0400

    format et validate

--- src/test/java/org/climoilou/vetm/MainTest.java
+++ src/test/java/org/climoilou/vetm/MainTest.java
@@ -15,96 +15,100 @@ class MainTest {
     void failTest1() {
         assertFalse(false);
     }
+
     @Test
     void failTest2() {
         assertFalse(false);
     }
+
     @Test
     void failTest3() {
         assertFalse(false);
     }
+
     @Test
     void failTest4() {
         assertFalse(false);
     }
+
     @Test
     void failTest5() {
         assertFalse(false);
     }
+
     @Test
     void failTest6() {
         assertFalse(false);
     }
+
     @Test
     void failTest7() {
         assertFalse(false);
     }
+
     @Test
     void failTest8() {
         assertFalse(false);
     }
+
     @Test
     void failTest9() {
         assertFalse(false);
     }
+
     @Test
     void failTest10() {
         assertFalse(false);
     }
+
     @Test
     void failTest11() {
         assertFalse(false);
     }
+
     @Test
     void failTest12() {
         assertFalse(false);
     }
+
     @Test
     void failTest13() {
         assertFalse(false);
     }
+
     @Test
     void failTest14() {
         assertFalse(false);
     }
+
     @Test
     void failTest15() {
         assertFalse(false);
     }
+
     @Test
     void failTest16() {
         assertFalse(false);
     }
+
     @Test
     void failTest17() {
         assertFalse(false);
     }
+
     @Test
     void failTest18() {
         assertFalse(false);
     }
+
     @Test
     void failTest19() {
         assertFalse(false);
     }
+
     @Test
     void failTest20() {
         assertFalse(false);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
